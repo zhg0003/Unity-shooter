7ba41477c4d9e2f0e0a11d6e81fb91c2388d26dd
zhg0003 <zhg0003@gmail.com>
2018-01-15 23:26:53 -0800

    init

---
+++ Assets/script/BGmanager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGmanager : MonoBehaviour {
+    public GameObject[] bg = new GameObject[3];
+    public float end = -18.9f;
+    public Vector2 begin = new Vector2(36.05f, 0.035f);
+    public Queue<GameObject> background; //THIS THING WON'T WORK WHYYY I DON'T WANNA USE LOOP D=
+	// Use this for initialization
+	void Start ()
+    {
+        //bg[0] = GameObject.FindGameObjectWithTag("");
+        //begin.x = 2*(bg[1].transform.position.x - bg[2].transform.position.x);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < 3; i++)
+        {
+            print((Vector2)bg[i].transform.position);
+            if (bg[i].transform.position.x <= end)
+            {              
+                bg[i].transform.position = begin;
+            }
+        }
+	}
+}
---
+++ Assets/script/moveBG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBG : MonoBehaviour {
+    public float speed = 0.5f; //smaller the faster
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+
+}
