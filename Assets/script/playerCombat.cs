de937f3badbb1c03bc1f9f86c6a01a9150d4dcdd
zhg0003 <zhg0003@gmail.com>
2018-01-17 00:17:29 -0800

    added player movement, combat partially done

---
+++ Assets/script/playerCombat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCombat : MonoBehaviour {
+
+    private bool attack;
+    private float timer;
+    private float attCD = 0.5f;
+    private Animator anim;
+    public GameObject bullet;
+    public float offset = 0.1f;
+    // Use this for initialization
+    void Awake () {
+        attack = false;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && attack == false) //attack when the fire button is pressed own and player is able to attack
+        {
+            anim.SetBool("att", true);
+            //instantiate bullet object
+            Vector3 pos = new Vector3(transform.position.x + offset, transform.position.y, 0);
+            Instantiate(bullet, pos, transform.rotation);
+            attack = true;
+            attCD = 0.5f;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            print("att detected");
+            anim.SetBool("att", false);
+        }
+
+        attCD -= Time.deltaTime;
+        if (attCD < 0)
+        {
+            attack = false;
+        }
+    }
+}
---
+++ Assets/script/playerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    public float speed = 1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //start with moving right 
+        if (Input.GetKey("right"))
+        {
+            movement(Time.deltaTime * speed, 0f);
+        }
+        if (Input.GetKey("left"))
+        {
+            movement(-Time.deltaTime * speed, 0f);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            movement(0f, Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            movement(0f, -Time.deltaTime * speed);
+        }
+    }
+
+    private void movement(float deltaX, float deltaY)
+    {
+        print("current transform position is " + transform.position);
+        Vector2 pos = new Vector2(transform.position.x + deltaX, transform.position.y+deltaY);
+        transform.position = pos;
+        print("moving to position" + pos);
+    }
+}
